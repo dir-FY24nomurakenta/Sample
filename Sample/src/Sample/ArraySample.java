2f0014ad7a2268e9c808e1baad7bd353aa7145e2
dir-FY24nomurakenta <KentaNomura()@Dirbato-1346x>
2024-04-09 09:30:32 +0900

    Lesson5 Chapter5

--- Sample/src/Sample/ArraySample.java
+++ Sample/src/Sample/ArraySample.java
@@ -3,8 +3,16 @@ package Sample;
 public class ArraySample {
 
     public static void main(String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
-
+        var numbers = new int[5];
+        
+        for (var i = 0; i < numbers.length; i++) {
+            numbers[i]= i * 2; //配列の各要素の値を代入 
+        }
+        
+        //配列の要素を表示
+        for (var i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
     }
 
 }
